35b5d1813820e3a06e5785f0a5df8095907a9d5b
newproject23 <143152902+newproject23@users.noreply.github.com>
2023-11-23 03:49:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
+    API_ID = 22204446
+    API_HASH = "d364e31d1d60893839d75eb214a7ec9e"
+    TOKEN = "6597762683:AAH99P3yjbb3MG1AEplsMnzPktu9ESyD4So"  
+    START_PIC = "https://telegra.ph/file/a6a76c1b792d094e87111.jpg" 
     CHAT = ""    
